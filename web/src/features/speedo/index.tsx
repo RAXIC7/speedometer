cfd4837987ffabb1fa5e88d421eff90f0d9d7a23
Snaily <snailycfx@gmail.com>
2023-04-20 10:11:43 +0200

    feat(features/speedo): edited stupid shit i made lol

--- web/src/features/speedo/index.tsx
+++ web/src/features/speedo/index.tsx
@@ -10,31 +10,14 @@ import './index.css'
 
 const Speedo: React.FC = () => {
     const [visible, setVisible] = React.useState<boolean>(false)
-    const [speed, setSpeed] = React.useState<number>(0)
-    const [gear, setGear] = React.useState<number>(0)
-    const [maxGear, setMaxGear] = React.useState<number>(0)
-    const [fuel, setFuel] = React.useState<number>(0)
-    const [maxFuel, setMaxFuel] = React.useState<number>(0)
+    const [data, setData] = React.useState<SpeedoProps>({ gear: 3, maxGear: 7, speed: 135, fuel: 50, maxFuel: 75 })
 
-    if (isEnvBrowser()) {
-        React.useEffect(() => {
-            setVisible(true)
-            setGear(3)
-            setMaxGear(7)
-            setSpeed(135)
-            setFuel(50)
-            setMaxFuel(75)
-        })
-    } else {
+    if (!isEnvBrowser()) {
         useNuiEvent('hideSpeedo', () => setVisible(false))
         useNuiEvent('updateSpeedo', (data: SpeedoProps) => {
             if (!visible) setVisible(true)
 
-            setGear(data.gear)
-            setMaxGear(data.maxGear)
-            setSpeed(data.speed)
-            setFuel(data.fuel)
-            setMaxFuel(data.maxFuel)
+            setData(data)
         })
     }
 
@@ -45,20 +28,20 @@ const Speedo: React.FC = () => {
                     <div className="speedo-container">
                         <div className="speedo-top">
                             <div className="speedo-speed">
-                                <span className="speedo-speed-number">{speed}</span>
+                                <span className="speedo-speed-number">{data.speed}</span>
                                 <span className="speedo-speed-text">mp/h</span>
                             </div>
 
                             <div className="speedo-gears">
-                                <span className="speedo-speed-gear">{gear}</span>
-                                <Progress progress={((gear / maxGear) * 100)} width="4vw" height=".3vw" borderRadius=".1vw" backgroundColor="rgb(155, 63, 63)"/>
+                                <span className="speedo-speed-gear">{data.gear}</span>
+                                <Progress progress={((data.gear / data.maxGear) * 100)} width="4vw" height=".3vw" borderRadius=".1vw" backgroundColor="rgb(155, 63, 63)"/>
                             </div>
                         </div>
 
                         <div className="speedo-bottom">
                             <div className="speedo-fuel">
                                 <BsFuelPumpFill className="speedo-fuel-icon" />
-                                <Progress progress={(fuel / maxFuel) * 100} width="7vw" height=".6vw" borderRadius=".3vw" text={fuel} backgroundColor="#ab70ff" />
+                                <Progress progress={(data.fuel / data.maxFuel) * 100} width="7vw" height=".6vw" borderRadius=".3vw" text={data.fuel} backgroundColor="#ab70ff" />
                             </div>
                         </div>
                     </div>
